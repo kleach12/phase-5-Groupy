78b0f9155def4ac3cf9fb8db8898c103ab36d44b
Kale Leach <kleach1212@gmail.com>
2023-02-19 22:00:49 -0800

    Commit 8: Switched to Postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,12 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_02_14_200811) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "profile_picture"
+    t.text "username"
+    t.text "profile_picture"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
