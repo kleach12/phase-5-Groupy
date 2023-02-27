6a5fcfb55323cd7cc13221ed1b86b00a6d484022
Kale Leach <kleach1212@gmail.com>
2023-02-27 14:36:08 -0800

    Commit 24: Added Avatar

--- client/src/Dashboard/DashNav/DashNav.js
+++ client/src/Dashboard/DashNav/DashNav.js
@@ -1,39 +1,45 @@
 import "./DashNav.css";
 import { Navigate } from "react-router-dom";
+import { BsInstagram, BsFacebook } from "react-icons/bs";
+import { useState } from "react";
+import Avatar from 'react-avatar';
 // import Button from "react-bootstrap/Button";
 
 export default function DashNav({ user, setUser, signedIn, setSignedIn }) {
 
-
-  function handleSignOut(){
-    fetch('/logout', {
-      method: "DELETE"
-    })
-    .then((res) => {
-      console.log(res)
-      setSignedIn(false)
-      setUser(null)
-      console.log(signedIn)
-    })
+  function handleSignOut() {
+    fetch("/logout", {
+      method: "DELETE",
+    }).then((res) => {
+      console.log(res);
+      setSignedIn(false);
+      setUser(null);
+      console.log(user.profile_picture)
+      console.log(signedIn);
+    });
   }
 
   if (signedIn === false) {
-    return (<Navigate to="/" />)
+    return <Navigate to="/" />;
   }
 
   return (
     <div id="dash_nav">
       <h1 id="title"> IRL </h1>
-      <img
-        id="profile_pic"
-        src="https://www.clipartmax.com/png/small/1-16702_world-clip-art-transparent-background-earth-clipart.png"
-        alt="profile pic"
-      />
+      {/* {userProfilePic} */}
+      {/* <img id="profile_pic" src={user.profile_picture} alt="profile pic" /> */}
+      <Avatar className="profile_pic" src = {user.profile_picture}name= {user.full_name}/>
       <div>
-        <h3 id="at"> @Irl</h3>
+        <h3 id="at"> @{user.username}</h3>
       </div>
-      <h3 id="sign_out" onClick={handleSignOut}>
-        {" "}
+
+      <div>
+        <h3 id="at"> Bio </h3>
+      </div>
+      <BsInstagram />
+      <BsFacebook />
+      <h3 className="dashnav_text"> Edit Profile </h3>
+      <h3 className="dashnav_text" onClick={handleSignOut}>
         Sign out
       </h3>
     </div>
