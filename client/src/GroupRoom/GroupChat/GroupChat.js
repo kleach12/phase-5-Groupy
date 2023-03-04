bb390e7b78786d4bcd9f09fdf60492eb589be186
Kale Leach <kleach1212@gmail.com>
2023-03-04 11:30:01 -0800

    Commit 36: Added Editable Div

--- client/src/GroupRoom/GroupChat/GroupChat.js
+++ client/src/GroupRoom/GroupChat/GroupChat.js
@@ -1,14 +1,42 @@
-import Button from "react-bootstrap/esm/Button";
+import { useRef, useState } from "react";
+// import Button from "react-bootstrap/esm/Button";
+import ContentEditable from "react-contenteditable";
 import "./GroupChat.css";
 
 export default function GroupChat() {
+  // const text = useRef('');
+  const formRef = useRef()
+  const [message, setMessage] = useState('')
+
+  const handleChange = (evt) => {
+    // console.log(evt.target.value)
+    setMessage(evt.target.value)
+  };
+
+  // const handleBlur = () => {
+  //   setMessage(text.current);
+  // };
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(message)
+    setMessage('')
+  }
+
   return (
     <div id="group_chat">
       <div id="groups_top">{/* this will be message area */}</div>
-        <form id="bottom_chat">
-          {/* <button> </button> */}
-          <div contentEditable="true" className="chat_box"></div>
-        </form>
+      <form id="bottom_chat" onSubmit={(e) => handleSubmit(e)} ref={formRef}>
+        {/* <button> </button> */}
+        <ContentEditable
+          html={message}
+          className="chat_box"
+          suppressContentEditableWarning={true}
+          // onBlur={handleBlur}
+          onChange={handleChange}
+        />
+        <button type="submit"> Submit </button>
+      </form>
       <div className="overflow"></div>
     </div>
   );
