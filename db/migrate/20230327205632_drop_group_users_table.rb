9e3ecfbf6a7da4d0f9b9bb9cfd72739ea5610aec
Kale Leach <kleach1212@gmail.com>
2023-04-17 13:34:41 -0700

    Commit 83: Deployment process

--- db/migrate/20230327205632_drop_group_users_table.rb
+++
@@ -1,5 +0,0 @@
-class DropGroupUsersTable < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :groupusers
-  end
-end
--- db/migrate/20230329162455_drop_join_table.rb
+++
@@ -1,5 +0,0 @@
-class DropJoinTable < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :groupusers
-  end
-end
