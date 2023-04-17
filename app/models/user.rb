a3eff35eea5212e03f1f33904d08b621a77d0fce
Kale Leach <kleach1212@gmail.com>
2023-04-17 14:16:37 -0700

    Commit 84: Fixed use validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,13 @@ class User < ApplicationRecord
   has_secure_password
   validates :username, :city, :image, presence: true
   validates :username, uniqueness: true
-  validates :email, uniqueness: true
+  # validates :email, uniqueness: true
   validates :username, length: {in: 1..15}
   validates :password, length: {minimum: 5}, on: :create #Smelly rails 
   validates :bio, length: {maximum: 250}
   validates :password, format: { with: /\A[A-Za-z0-9#@$%^&+=]+\z/, message: "only allows letters, numbers, and symbols #@$%^&+=" },on: :create
-  validates :first_name, format: { with: /\A[A-Za-z0-9#@$%^&+=]+\z/, message: "only allows letters, numbers, and symbols #@$%^&+=" }
-  validates :last_name, format: { with: /\A[A-Za-z0-9#@$%^&+=]+\z/, message: "only allows letters, numbers, and symbols #@$%^&+=" }
+  # validates :first_name, format: { with: /\A[A-Za-z0-9#@$%^&+=]+\z/, message: "only allows letters, numbers, and symbols #@$%^&+=" }
+  # validates :last_name, format: { with: /\A[A-Za-z0-9#@$%^&+=]+\z/, message: "only allows letters, numbers, and symbols #@$%^&+=" }
   validates :username, format: { with: /\A[A-Za-z0-9#@$%^&+=]+\z/, message: "only allows letters, numbers, and symbols #@$%^&+=" }
 
   has_many :group_users, dependent: :destroy
