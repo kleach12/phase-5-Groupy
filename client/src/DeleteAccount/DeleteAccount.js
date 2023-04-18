810a0b8ad417db81904843e1113f2bec9516b817
Kale Leach <kleach1212@gmail.com>
2023-04-18 10:09:02 -0700

    Commit User groups clear on delete

--- client/src/DeleteAccount/DeleteAccount.js
+++ client/src/DeleteAccount/DeleteAccount.js
@@ -4,12 +4,13 @@ import { BsFillArrowLeftSquareFill } from "react-icons/bs";
 import { useContext } from 'react';
 import { AllContext } from '../AllContext';
 export default function DeleteAccount(){
-const {deleteUser, setDeleteUser, signedIn, setSignedIn, setTheme} = useContext(AllContext)
+const {deleteUser, setDeleteUser, signedIn, setSignedIn, setTheme, setUserGroups} = useContext(AllContext)
   function handleDeleteUser() {
     fetch(`/api/delete_user`, {
       method: "DELETE",
     }).then((res) => {
       console.log(res);
+      setUserGroups([])
       setTheme('light')
       setSignedIn(false);
       setDeleteUser(false)
