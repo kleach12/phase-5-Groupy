112a357709ff6c5f2aedfa870eed797324e34f89
Kale Leach <kleach1212@gmail.com>
2023-04-18 14:05:22 -0700

    Commit 92: Fixed create account dropdown

--- client/src/CityDropdown/CityDropdown.js
+++ client/src/CityDropdown/CityDropdown.js
@@ -5,7 +5,7 @@ import { AllContext } from '../AllContext';
 import { useContext } from 'react';
 
 export default function CityDropdown({setCity, city}){
-  const {theme, user} =  useContext(AllContext)
+  const {theme, user, signedIn} =  useContext(AllContext)
   
   if (theme){
   const colorStyles =  theme === 'light' ? {
@@ -51,9 +51,9 @@ export default function CityDropdown({setCity, city}){
             className="sign_up_select"
             options={cities}
             onChange={(e) => setCity(e.value)}
-            placeholder={user ? user.city : `Select city...`}
+            placeholder={signedIn ? user.city : `Select city...`}
             styles= {colorStyles}
-            value={city}
+            value={signedIn ? city : null}
           />
   )
   }
