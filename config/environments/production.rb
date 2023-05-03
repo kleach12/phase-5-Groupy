9a16f78d965c8d19e9c249170cb0e139658e3a6d
Kale Leach <kleach1212@gmail.com>
2023-05-02 20:45:25 -0700

    Commit 120: Testing config stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,9 @@ Rails.application.configure do
     # Store uploaded files on the local file system (see config/storage.yml for options).
     config.active_storage.service = :render
     
-    # config.action_cable.allowed_request_origins = ["https://irl.onrender.com"]
+    config.action_cable.url = 'wss://https://irl.onrender.com/cable'
+
+    config.action_cable.allowed_request_origins = ["https://irl.onrender.com"]
 
     config.action_cable.disable_request_forgery_protection = true
 end
