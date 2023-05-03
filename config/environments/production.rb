495682b78c027a6c83c91827756d7a64482f8705
Kale Leach <kleach1212@gmail.com>
2023-05-03 13:33:22 -0700

    Commit 133: Testing out action cable url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
     # Store uploaded files on the local file system (see config/storage.yml for options).
     config.active_storage.service = :render
     
-    # config.action_cable.url = 'wss://https://irl.onrender.com/cable'
+    config.action_cable.url = 'wss://https://irl.onrender.com/cable'
 
     # config.action_cable.allowed_request_origins = ["https://irl.onrender.com"]
 
